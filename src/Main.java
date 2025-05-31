47c9fd50180ad33ef966a2ec4122e083cfcf16ba
mgvrez <maxilona2024@gmail.com>
2025-05-31 23:47:12 +0300

    src

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
 
-        int cost;
-        cost = 13676;
-        int bonusMile;
-        bonusMile = cost / 20;
+        int cost = 13676;
+        int bonusMile = cost / 20;
         System.out.println("Бонусные мили:" + bonusMile);
 
 
